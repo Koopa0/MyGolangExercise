05d10aff84506c5e34128df9b8a72e8a6efe510e
koopa <koopapapa@gmail.com>
2020-11-09 09:07:52 +0800

    first commit

---
+++ MyGolang/golang-web-dev/000_temp/03_christmas/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+func main()  {
+	tpl, err := template.ParseFiles("letter.phpasp")
+	if err != nil{
+		fmt.Println("There was an error parsing file", err)
+	}
+	friend := []string{"Alex", "Conor","Ken","Ronnie","Patick","Nina","Jeremy","Gentry","Christian"}
+
+	err = tpl.Execute(os.Stdout, friend)
+	if err != nil {
+		fmt.Println("There was an error executing template", err)
+	}
+}
