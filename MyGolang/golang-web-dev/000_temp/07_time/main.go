f60233c2fe5dbca82628abed75a09afe4fcf562c
koopa <koopapapa@gmail.com>
2020-11-09 11:42:56 +0800

    first commit

---
+++ MyGolang/golang-web-dev/000_temp/07_time/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	t1 := time.Now()
+	//fmt.Println(t1)
+	fmt.Println(t1.Format("03:04"))
+
+	t2 := time.Now().Add(2 * time.Minute)
+	fmt.Println(t2.Format("03:04"))
+
+	fmt.Println(t1.Before(t2))
+}
